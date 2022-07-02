e686d5aa5c6e8806761b2be2910713f2f84560f8
Yerin-No <w2006@e-mirim.hs.kr>
2022-07-03 00:35:08 +0900

    urls uniform:detail

--- uniform/urls.py
+++ uniform/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from uniform.views import UniformListView, UniformCreateView
+from uniform.views import UniformListView, UniformCreateView, UniformDetailView
 
 app_name = 'uniform'
 
 urlpatterns = [
     path('list/', UniformListView.as_view(), name='list'),   # uniform:list
     path('add/', UniformCreateView.as_view(), name='add'),  # uniform:add
+    path('detail/<int:pk>/', UniformDetailView.as_view(), name='detail'),   # uniform:detail
 ]
