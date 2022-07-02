cbf8787a2d312e06f5e344bd2def6aba9f847dad
Yerin-No <w2006@e-mirim.hs.kr>
2022-07-03 00:45:46 +0900

    home link

--- UniformBank/urls.py
+++ UniformBank/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from uniform.views import UniformListView
+
 urlpatterns = [
+    path('', UniformListView.as_view(), name='home'),
     path('uniform/', include('uniform.urls')),
     path('admin/', admin.site.urls),
 ]
