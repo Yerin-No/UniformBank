59a84602d1ae0c821a6fc9833a9706bd0dc6c805
Yerin-No <w2006@e-mirim.hs.kr>
2022-07-03 00:19:23 +0900

    urls uniform:Add

--- uniform/urls.py
+++ uniform/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from uniform.views import UniformListView
+from uniform.views import UniformListView, UniformCreateView
 
 app_name = 'uniform'
 
 urlpatterns = [
-    path('list/', UniformListView.as_view(), name='list')   # uniform:list
+    path('list/', UniformListView.as_view(), name='list'),   # uniform:list
+    path('add/', UniformCreateView.as_view(), name='add'),  # uniform:add
 ]
